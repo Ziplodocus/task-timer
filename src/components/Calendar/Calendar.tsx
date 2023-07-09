92ae248592cf88085c57355800ae62507cee452c
Ziplodocus <zirpho@gmail.com>
2023-07-09 12:04:02 +0100

    Accurately place the tasks within the time range ( UTC ) and make responsive with range inputs

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,4 +1,4 @@
-import { Component, For } from "solid-js";
+import { Component, createSignal, For, Index } from "solid-js";
 import { createStore } from "solid-js/store";
 import * as Storage from "../../Storage/local";
 import { durationInS } from "../../Structs/Entry";
@@ -12,61 +12,87 @@ const Calendar: Component = () => {
   const list = Storage.get("tasks") ?? [create("Hehe")];
   const [tasks, setTasks] = createStore<Task[]>(list);
 
-  const TimeFrame = {
-    start: 0,
-    steps: 24,
-    stepRange: 3600,
-    range() {
-      return this.steps * this.stepRange;
-    }
+  const stepRange = 3600;
+  const [getRangeStart, setRangeStart] = createSignal(0);
+  const [getRangeEnd, setRangeEnd] = createSignal(24);
+
+  const getRangeSteps = () => {
+    return (getRangeEnd() - getRangeStart());
+  }
+
+  const getRangeInSeconds = () => {
+    return getRangeSteps() * stepRange;
   }
 
   function getStartPosition(startTimeUnix: number) {
-    const startSecondInDay = startTimeUnix % (3600 * 24);
-    const startPercent = startSecondInDay / TimeFrame.range() * 100;
+    const taskDate = new Date(startTimeUnix);
+    const startOfDay = new Date(taskDate.getUTCFullYear(), taskDate.getUTCMonth(), taskDate.getUTCDate());
+    const startSecondInDay = (taskDate.valueOf() - startOfDay.valueOf()) / 1000;
+    const startSecondInRange = startSecondInDay - getRangeStart() * 3600;
+    const startPercent = startSecondInRange / getRangeInSeconds() * 100;
     return startPercent;
   };
 
   function getPercentageDuration(durationInSeconds: number) {
-    return durationInSeconds / TimeFrame.range() * 100;
+    return durationInSeconds / getRangeInSeconds() * 100;
+  }
+
+  const onRangeUpdate = (e: InputEvent) => {
+    const input = e.currentTarget;
+    const name = input.name;
+    switch (name) {
+      case 'min-time':
+        setRangeStart(parseInt(input.value));
+        break;
+      case 'max-time':
+        setRangeEnd(parseInt(input.value));
+        break;
+    }
   }
 
   return (
-    <section class={styles.calendar}>
-      <div class={styles.hours}>
-        {
-          range(TimeFrame.steps).map(i => {
-            const formattedTime = formatSeconds(TimeFrame.start + TimeFrame.stepRange * i);
-            return (
-              <div class={styles.hour} >{formattedTime}</div>
-            )
-          })
-        }
-      </div>
+    <>
+      <header>
+        <h2>View</h2>
+        <label>Time Start <input name="min-time" type="number" min="0" max="23" value={getRangeStart()} oninput={onRangeUpdate} /></label>
+        <label>Time End <input name="max-time" type="number" min="1" max="24" value={getRangeEnd()} oninput={onRangeUpdate} /></label>
+      </header>
 
-      <ul class={styles.tasks}>
-        <For each={tasks}>
-          {task => {
-            return (
-              <For each={task.entries}>
-                {entry => {
-                  return (
-                    <li class={styles.entry} style={{
-                      "top": `${getStartPosition(entry.start)}%`,
-                      "height": `${getPercentageDuration(durationInS(entry))}%`
-                    }}>
-                      <span class={styles.entryName}>{task.name}</span>
-                      <span class={styles.entryDuration}>{formatSeconds(durationInS(entry))}</span>
-                    </li>
-                  )
-                }}
-              </For>
-            )
-          }}
-        </For>
-      </ul>
-    </section>
+      <section class={styles.calendar}>
+        <div class={styles.hours}>
+          {
+            range(getRangeSteps() + 1).map(i => {
+              const formattedTime = (getRangeStart() + i) % 24 + ':00';
+              return (
+                <div class={styles.hour} >{formattedTime}</div>
+              )
+            })
+          }
+        </div>
 
+        <ul class={styles.tasks}>
+          <For each={tasks}>
+            {task => {
+              return (
+                <For each={task.entries}>
+                  {entry => {
+                    return (
+                      <li class={styles.entry} style={{
+                        "top": `${getStartPosition(entry.start)}%`,
+                        "height": `${getPercentageDuration(durationInS(entry))}%`
+                      }}>
+                        <span class={styles.entryName}>{task.name}</span>
+                        <span class={styles.entryDuration}>{formatSeconds(durationInS(entry))}</span>
+                      </li>
+                    )
+                  }}
+                </For>
+              )
+            }}
+          </For>
+        </ul>
+      </section>
+    </>
   )
 }
 
--- src/utilities/timeconversion.ts
+++ src/utilities/timeconversion.ts
@@ -15,3 +15,8 @@ export function formatSeconds (seconds: number): string {
 
     return timeString;
 };
+
+export function getSecondsToday() {
+  let d = new Date();
+  return d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds();
+}
